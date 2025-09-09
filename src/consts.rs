2c77511acabde61d49e5bb84af7b0d226115440c
etcetera <hubertetcetera@gmail.com>
2025-09-09 18:34:59 +0530

    feat(consts.rs): add consts file

---
+++ src/consts.rs
@@ -0,0 +1,5 @@
+pub const ABOUT_CLI: &'static str = "A minimal git cli implemented in Rust";
+pub const ABOUT_INIT: &'static str =
+	"Create an empty Git repository or reinitialize an existing one";
+pub const ABOUT_CATFILE: &'static str =
+	"Provide content or type and size information for repository objects";
