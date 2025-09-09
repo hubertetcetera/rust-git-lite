015bf2b24a06c9630b4602610a7fafde5a6df214
etcetera <hubertetcetera@gmail.com>
2025-09-09 16:27:52 +0530

    feat(main.rs): add Command enums and helper functions scaffold

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,49 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+use std::str::FromStr;
+use strum::{Display, EnumString};
 
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
-    }
+#[derive(Default, Display, EnumString)]
+#[strum(serialize_all = "kebab-case")]
+enum Command {
+	Init,
+	CatFile,
+	#[default]
+	Help,
+}
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() < 2 {
+		return help()
+	}
+
+	let Ok(command) = Command::from_str(args[1].as_str()) else {
+		eprintln!("Unknown command: {}", args[1]);
+		return help()
+	};
+
+	match command {
+		Command::Init => init(),
+		Command::CatFile => cat_file(),
+		Command::Help => help(),
+	}
+}
+
+fn init() {
+	fs::create_dir(".git").unwrap();
+	fs::create_dir(".git/objects").unwrap();
+	fs::create_dir(".git/refs").unwrap();
+	fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+	println!("Initialized git directory")
+}
+
+fn cat_file() {
+	println!("running cat-file")
+}
+
+fn help() {
+	println!("Usage: rust-git-lite COMMAND <flags>")
 }
