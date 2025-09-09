b57c42ba0c3bbe0e9260ec7f08becdf757e1ca7d
etcetera <hubertetcetera@gmail.com>
2025-09-09 18:56:04 +0530

    refactor(main.rs): use functions from helper module

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
-use std::fs;
-
 use clap::Parser;
 
 mod consts;
+mod helpers;
 
 #[derive(Debug, Parser)]
 #[command(about = consts::ABOUT_CLI)]
 enum Command {
-    #[command(about = consts::ABOUT_INIT)]
+	#[command(about = consts::ABOUT_INIT)]
 	Init,
-    #[command(about = consts::ABOUT_CATFILE)]
+	#[command(about = consts::ABOUT_CATFILE)]
 	CatFile,
 }
 
 fn main() {
-    let cmd = Command::parse();
+	let cmd = Command::parse();
 
-    match cmd {
-        Command::Init => init(),
-        Command::CatFile => cat_file(),
-    }
-}
-
-fn init() {
-	fs::create_dir(".git").unwrap();
-	fs::create_dir(".git/objects").unwrap();
-	fs::create_dir(".git/refs").unwrap();
-	fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-	println!("Initialized git directory")
-}
-
-fn cat_file() {
-	println!("running cat-file")
+	match cmd {
+		Command::Init => helpers::init(),
+		Command::CatFile => helpers::cat_file(),
+	}
 }
