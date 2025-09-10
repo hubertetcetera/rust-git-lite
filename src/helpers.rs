7e4d15b33a57d7a298d4ca1031b64e1f4c81d518
etcetera <hubertetcetera@gmail.com>
2025-09-10 19:15:48 +0530

    feat(helpers.rs): read object and print decompressed content

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,9 @@
-use std::fs;
+use std::{
+	fs,
+	io::{Cursor, Read},
+};
+
+use flate2::read::ZlibDecoder;
 
 use crate::commands::CatFileArgs;
 
@@ -13,10 +18,23 @@ pub fn init() {
 pub fn cat_file(args: CatFileArgs) {
 	eprintln!("\nrunning cat-file with args: {:#?}\n", args);
 	let (dir, file) = args.object.split_at(2);
-	let path = ".git/objects/".to_string() + dir + "/" + file;
-	let Ok(buf) = fs::read(&path).map_err(|e| eprintln!("Error while reading {path}: {e}\n"))
+	let path = format!(".git/objects/{dir}/{file}");
+	let Ok(compressed) =
+		fs::read(&path).map_err(|e| eprintln!("Error while reading {path}: {e}\n"))
 	else {
 		return;
 	};
-	println!("{:?}", buf)
+	let mut decoder = ZlibDecoder::new(Cursor::new(compressed));
+	let mut content = String::new();
+	let Ok(_) = decoder
+		.read_to_string(&mut content)
+		.map_err(|e| eprintln!("Error while writing content to string: {e}"))
+	else {
+		return;
+	};
+	let Some(null_pos) = content.find('\0') else {
+		eprintln!("Failed to find null_pos: `content.find('\\0')` returned `None`");
+		return;
+	};
+	print!("{}", content.split_off(null_pos + 1))
 }
