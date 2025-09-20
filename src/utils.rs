38037ce07f622bac08385d2d61af9542e6b1c3af
etcetera <hubertetcetera@gmail.com>
2025-09-20 17:06:20 +0530

    chore(utils): add todos

--- src/utils.rs
+++ src/utils.rs
@@ -48,14 +48,16 @@ pub fn zlib_decode(path: &PathBuf) -> Result<String> {
 /// `<size>` is the size of the content (in bytes)
 /// `\0` is a null byte
 /// `<content>` is the actual content of the file which the function returns.
-/// 
+///
 /// TODO: add check for ensuring header isn't malformed
 pub fn strip_header(content: &String) -> Result<String> {
-	let null_pos = content.find('\0').with_context(|| "find NUL after header:")?; // Get the position of the null byte (`\0`)
-	Ok(content.clone().split_off(null_pos + 1))
+	let nul_pos = content.find('\0').with_context(|| "find NUL after header:")?; // Get the position of the null byte (`\0`)
+	Ok(content.clone().split_off(nul_pos + 1))
 }
 
+/// TODO: add documentation
 pub fn ensure_object_header_type(content: &String, expected_type: &str) -> Result<()> {
+	// TODO: change content type to `&str`
 	if !content.starts_with(expected_type) {
 		anyhow::bail!("not a tree object: header doesn't start with `tree` keyword")
 	}
